4bcd3458f4c3b9afbc27e66644e299fd41cc4c30
Stephen Garver <stiv3n@pm.me>
2022-01-25 18:52:41 -0800

    Add implementation for findPersonById

--- myApp.js
+++ myApp.js
@@ -48,7 +48,14 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+
+    Person.findOne({_id: personId}, (err, data) => {
+        if (err) {
+            done(err);
+        } else {
+            done(null, data);
+        }
+    });
 };
 
 const findEditThenSave = (personId, done) => {
